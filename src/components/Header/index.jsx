96cbb3734de8580bb1ab19ae59607a7b33d4a3a6
Rafael Afonso <122814044+rafae1-afonso@users.noreply.github.com>
2025-07-16 19:22:08 -0300

    Criação do Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,53 @@
+import logoImage from '../../images/logo.png'
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    padding: 10px 31px;
+`;
+
+const StyledImage = styled.img`
+    width: 35px;
+    border-radius: 10px;
+`
+
+const StyledForm = styled.form`
+    display: flex;
+    justify-content: start;
+    width: 500px;
+    border-radius: 50px;
+    border: 1px solid #DDDDDD;
+    padding: 10px 20px;
+`
+
+const SearchInput = styled.input`
+    width: 100%;
+    border: none;
+    
+
+    &:focus {
+        outline: none;
+    }
+
+    &::placeholder {
+        color: #999999;
+    }
+`
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <StyledImage src={logoImage} />
+            <StyledForm>
+                <SearchInput type='search' placeholder='Pesquisar Prato' />
+            </StyledForm>
+            <div></div>
+        </StyledHeader>
+    )
+}
+
+export default Header
